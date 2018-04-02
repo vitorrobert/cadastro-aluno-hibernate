71afaa23b69e775966c6f0a66e91175bbdb11e8d
04223376250 <04223376250@JIPA-LABIN03-15.ifro.local>
2018-04-02 14:26:52 -0400

    Fazendo Instalação do Hibernate

---
+++ CadastroAlunoHibernate/src/br/edu/ifro/modelo/GeneretionType.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifro.modelo;
+
+/**
+ *
+ * @author 04223376250
+ */
+class GeneretionType {
+    
+}
